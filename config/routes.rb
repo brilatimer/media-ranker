7a66d6c7aed76c8bc30dc331598fb3c716ff48d1
brilatimer <brihiramine@gmail.com>
2019-10-16 14:15:23 -0700

    add new user option on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#index'
   resources :works
-
-
+  
+  
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
-
+  
   get '/users', to: 'users#index', as: 'users'
-  get '/users/new', to: 'users#new', as: 'new_book'
+  get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
-
+  
 end
