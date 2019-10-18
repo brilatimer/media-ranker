1858af91ebc31d3945aeae122b0f16392c9a83e8
brilatimer <brihiramine@gmail.com>
2019-10-18 15:33:05 -0700

    vote sorting setup in works.rb

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,11 @@
 class Work < ApplicationRecord
   has_many :votes
+  
+  def self.featured
+    featured_work = Work.all.max_by do |work|
+      work.votes.count 
+    end
+    return featured_work
+  end
 end
 
