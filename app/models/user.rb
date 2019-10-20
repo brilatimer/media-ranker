311604f4bd38ed29493d878d51b5b97d0ffe0275
brilatimer <brihiramine@gmail.com>
2019-10-20 14:13:57 -0700

    user vote history page working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  def works_by_user
+    votes = Vote.where(user_id: self.id)
+    works_list = []
+    votes.each do |vote|
+      works_list  << Work.find_by(id: vote.work_id)
+    end
+    return works_list
+  end
 end
 
