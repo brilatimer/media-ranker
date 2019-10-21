e76b4b1bdaafb1782519f10309187f4541763790
brilatimer <brihiramine@gmail.com>
2019-10-20 18:54:50 -0700

    model for vote testing complete

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  describe Vote do
+    let (:new_vote) {
+    work_one = Work.new(category: "movie", title: "Mulan", creator: "disney", publication_year: 1990, description: "girl who loves books falls in love with prince")
+    user_one = User.new(username: "muffin")
+    
+    new_vote = Vote.new(user: user_one, work: work_one)
+  }
+  
+  describe "validations" do
+    it "must have a user" do
+      # Arrange
+      new_vote.user = nil
+      
+      # Assert
+      expect(new_vote.valid?).must_equal false
+      expect(new_vote.errors.messages).must_include :user
+      expect(new_vote.errors.messages[:user]).must_equal ["must exist", "can't be blank"]
+    end
+  end
+end
 end
